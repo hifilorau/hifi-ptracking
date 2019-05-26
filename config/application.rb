3678ebdaae28d825c740d83d7e27faa200b29ab5
EC2 Default User <ec2-user@ip-172-31-28-173.us-west-2.compute.internal>
2019-05-26 13:18:21 +0000

    bug fix with webconsole

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module HifiTrack
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.web_console.whitelisted_ips = '76.182.36.232'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
